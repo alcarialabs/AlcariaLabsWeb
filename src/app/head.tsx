6386ec9120705f39ff24128e05c38442db4f4851
Joan Aparici Morant <japarici@externos.ordenacionjuego.gob.es>
2025-02-07 18:06:51 +0100

    Fix: estructura y configuración para Next.js 13+

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Free Next.js Template for Startup and SaaS</title>
+      <meta name="description" content="This is Home for Startup Nextjs Template" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
