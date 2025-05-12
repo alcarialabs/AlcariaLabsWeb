3c81434ddfaebb171456e0d6550b04a193a465dd
ericduro <eriic11duro@gmail.com>
2025-05-12 20:19:19 +0200

    Solucionando sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://alcarialabs.com', // ← tu dominio principal con https
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+}
